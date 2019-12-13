69060b2612e339b4b6292dded597a90e71ee4df1
slavb <slavb@ilb.ru>
2019-12-13 09:06:38 +0000

    fix release script

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
-mvn release:prepare release:perform
-cd target/checkout && mvn deploy -P'!local-repository,release-profile'
+mvn deploy -P'!local-repository,release-profile,sonatype-repository'
+#mvn release:prepare release:perform
+#cd target/checkout && mvn deploy -P'!local-repository,release-profile'
 echo "Открыть https://oss.sonatype.org/#stagingRepositories"
 echo "отметить ruilb-XXXX, Close -> Confirm, подождать нажимая Refresh, пока Activity не станет Last operation completed successfully, нажать Release -> Confirm"
